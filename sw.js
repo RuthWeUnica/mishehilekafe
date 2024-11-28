e92ebaa0f8c012a4e5eb2aa7cf104d16981768bd
Miri-Shtul <miri176726@gmail.com>
2024-11-28 10:52:59 +0200

    update firebase version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.20.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-messaging.js');
 
 const firebaseConfig = {
   apiKey: "AIzaSyDSllBt0vuMT8VboviUDRKF3k1XyheQPcs",
