666aecd4d92b97948be303d5e0de90495065ce06
Miri-Shtul <miri176726@gmail.com>
2024-11-28 16:58:45 +0200

    change order of calling3

--- script.js
+++ script.js
@@ -10,13 +10,33 @@
 //     });
 // }
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";// TODO: Add SDKs for Firebase products that you want to use
+import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging.js";
 
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  if ('serviceWorker' in navigator && 'PushManager' in window) {
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDSllBt0vuMT8VboviUDRKF3k1XyheQPcs",
+    authDomain: "push-notifications-weunica.firebaseapp.com",
+    projectId: "push-notifications-weunica",
+    storageBucket: "push-notifications-weunica.firebasestorage.app",
+    messagingSenderId: "1042180147223",
+    appId: "1:1042180147223:web:3d73406c082f45dcac3452"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('sw.js', { type: 'module' })
             .then(registration => {
                 console.log('Service Worker registered successfully: ', registration);
+
+
+                const messaging = getMessaging(app);
             })
             .catch(error => {
                 console.log('Service Worker registration failed: ', error);
