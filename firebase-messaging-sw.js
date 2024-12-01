382cb4d0df3e4f01527801cf1a4cf4b0d97cc5c7
Miri-Shtul <miri176726@gmail.com>
2024-12-01 12:21:29 +0200

    correct get token

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getMessaging, onBackgroundMessage } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-sw.js";
+import { getMessaging, onBackgroundMessage, getToken } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-sw.js";
 
 // Firebase configuration
 const firebaseConfig = {
@@ -27,11 +27,27 @@ onBackgroundMessage(messaging, (payload) => {
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-messaging.getToken({ vapidKey: 'BKJdFLRgiRiRRNcNlQvRUDv15OIPDtaeXrIfeUClN9whgM1E1WIMt4AZlP8SPeV9vF1R6I3EZGO_OWyHzJTw73g' }).then((currentToken) => {
+// messaging.getToken({ vapidKey: 'BKJdFLRgiRiRRNcNlQvRUDv15OIPDtaeXrIfeUClN9whgM1E1WIMt4AZlP8SPeV9vF1R6I3EZGO_OWyHzJTw73g' }).then((currentToken) => {
+//   if (currentToken) {
+//     console.log("current token",currentToken);
+//     // Send the token to your server and update the UI if necessary
+//     // ...
+//   } else {
+//     // Show permission request UI
+//     console.log('No registration token available. Request permission to generate one.');
+//     // ...
+//   }
+// }).catch((err) => {
+//   console.log('An error occurred while retrieving token. ', err);
+//   // ...
+// });
+
+getToken(messaging, { vapidKey: 'BKJdFLRgiRiRRNcNlQvRUDv15OIPDtaeXrIfeUClN9whgM1E1WIMt4AZlP8SPeV9vF1R6I3EZGO_OWyHzJTw73g' }).then((currentToken) => {
   if (currentToken) {
-    console.log("current token",currentToken);
     // Send the token to your server and update the UI if necessary
     // ...
+    console.log("current token", currentToken);
+
   } else {
     // Show permission request UI
     console.log('No registration token available. Request permission to generate one.');
