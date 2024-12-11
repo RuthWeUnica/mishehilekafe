5b77acc26d0443f0e7866a1012b29ac20595d325
RuthWeUnica <ruth.k@weunica.com>
2024-12-11 04:28:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -53,13 +53,7 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
 } else {
     console.log('Push notifications are not supported in this browser.');
 }
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-        console.log('Service Worker registered with scope:', registration.scope);
-    }).catch(function(error) {
-        console.log('Service Worker registration failed:', error);
-    });
-}
+
 // Function to get the FCM Token
 function getFCMToken() {
     console.log("----> getFCMToken");
