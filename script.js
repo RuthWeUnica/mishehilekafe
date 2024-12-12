3d01a40d10d278ca5e0dc020fb69d99e3ea00624
RuthWeUnica <ruth.k@weunica.com>
2024-12-12 16:33:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ console.log("messeging in script.js", messaging);
 // Request Notification Permission
 Notification.requestPermission().then(permission => {
     if (permission === "granted") {
-        console.log("Notification permission granted.");
+        console.log("Notification permission granted!!!.");
         // getFCMToken();
     } else {
         console.log("Notification permission denied.");
