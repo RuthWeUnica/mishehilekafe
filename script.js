250ba2628c90cac5bd6fb0d77626cad44ab0d0ac
RuthWeUnica <ruth.k@weunica.com>
2024-12-12 12:28:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -77,9 +77,9 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
 
 // טעינת ה-iframe רק לאחר שה-`service worker` נרשם
 function loadIframe() {
-    console.log("come into load iframe:)))))))))))))");
+    console.log("come into load iframe:((((((9");
   const iframe = document.getElementById('wix-iframe');
-  iframe.src = "https://www.mishehilekafe.co.il/"; // הצב כאן את ה-URL הרצוי ל-iframe
+  iframe.src = "https://ruthweunica.github.io/mishehilekafe/"; // הצב כאן את ה-URL הרצוי ל-iframe
  console.log("switch to misheilekafeeeeeeeeee");
    
 }
