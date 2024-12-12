00efdecb959f50ee0d59c40dda4216bad5dc375b
RuthWeUnica <ruth.k@weunica.com>
2024-12-12 13:26:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
         navigator.serviceWorker.register('firebase-messaging-sw.js', { type: 'module' })
             .then((registration) => {
                    loadIframe();
-                sendMessageToServiceWorker();
+                // sendMessageToServiceWorker();
                 console.log('Firebase Messaging Service Worker registered successfully:', registration);
                 getFCMToken();
           
