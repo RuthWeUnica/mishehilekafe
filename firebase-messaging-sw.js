5c66bfa9b8687ab6f1b24ab15a63fbff936bf032
RuthWeUnica <ruth.k@weunica.com>
2024-12-15 13:13:07 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -40,24 +40,25 @@ self.addEventListener('fetch', fetchEvent => {
     // בדיקה אם הבקשה היא עבור ה-iframe
     if (requestUrl === 'https://ruthweunica.github.io/mishehilekafe/') {
         fetchEvent.respondWith(
-            fetch('https://www.mishehilekafe.co.il/')
-            .then(response => {
-                // חזור עם התגובה
-                return response;
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                return new Response('Error fetching data', { status: 500 });
-            })
+              fetch("https://www.mishehilekafe.co.il/")
+                .then(response => response)
+                .catch(error => {
+                    console.log('Error fetching alternate URL:', error);
+                    return new Response("Failed to fetch alternate URL.", { status: 500 });
+                })
+          
         );
     }
 });
-     // fetch("https://www.mishehilekafe.co.il/")
-            //     .then(response => response)
-            //     .catch(error => {
-            //         console.log('Error fetching alternate URL:', error);
-            //         return new Response("Failed to fetch alternate URL.", { status: 500 });
-            //     })
+   // fetch('https://www.mishehilekafe.co.il/')
+            // .then(response => {
+            //     // חזור עם התגובה
+            //     return response;
+            // })
+            // .catch(error => {
+            //     console.error('Error:', error);
+            //     return new Response('Error fetching data', { status: 500 });
+            // })  
 // מאזין לאירוע message לעדכון כתובת היעד
 // self.addEventListener('message', event => {
 //     console.log("cammmmmmmmmmmmmmmmmeeeeeeeeee:))))");
