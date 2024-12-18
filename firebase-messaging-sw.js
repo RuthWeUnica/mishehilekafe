1f9069cd91f5fdf92a2d15d63210250201f12245
Miri-Shtul <miri176726@gmail.com>
2024-12-18 13:02:38 +0200

    on background event3

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -52,6 +52,6 @@ onBackgroundMessage(messaging, (payload) => {
 self.addEventListener('notificationclick', function (event) {
   console.log("notification clicked!", event);
   event.notification.close(); // סוגר את הנוטיפיקציה
-  clients.openWindow("https://www.mishehilekafe.co.il/");
+  clients.openWindow("/index.html");
 
 });
